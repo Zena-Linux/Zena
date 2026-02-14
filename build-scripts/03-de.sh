58b6ba034d5225d2cc7ca9df57fff66d240711ae
JianZcar <esteban.jianzcar@outlook.com>
2026-02-15 01:52:35 +0800

    use releasever

--- build-scripts/03-de.sh
+++ build-scripts/03-de.sh
@@ -36,7 +36,7 @@ packages=(
 )
 
 dnf5 -y install "${packages[@]}" --setopt=install_weak_deps=False
-dnf5 install matugen --enablerepo=fedora-44 --disablerepo='*copr*'
+dnf5 -y install matugen --releasever=44 --disablerepo='*copr*'
 
 # Uninstall
 packages=(
