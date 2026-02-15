d61b24d6f88a81965560c2360d0c7cd187ec5a1f
JianZcar <esteban.jianzcar@outlook.com>
2026-02-15 12:47:12 +0800

    use systemd-run to start the gui instance

--- system-files/base/usr/libexec/run-zena-setup.sh
+++ system-files/base/usr/libexec/run-zena-setup.sh
@@ -10,5 +10,17 @@ if ! id "$SETUP_USER" &>/dev/null; then
     useradd --system --shell /usr/bin/bash "$SETUP_USER"
 fi
 
+chvt 1
+systemctl stop getty@tty1.service
+systemd-run --unit=run-zena-setup-gui --service-type=oneshot \
+  --description="Zena Setup" \
+  --property=StandardInput=tty \
+  --property=TTYPath=/dev/tty1 \
+  --property=User=root \
+  --property=Before=greetd.service \
+  --property=After=home.mount \
+  bash -c '
+    exec su - zena-setup -c "exec /usr/bin/niri --config /etc/zena-setup/niri.kdl"' || true
+
 touch /var/lib/zena-setup.done
-runuser -u "$SETUP_USER" -- "$NIRI_CMD" --config /etc/zena-setup/niri.kdl
+userdel -r zena-setup
