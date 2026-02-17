87de4b0159dd5772ba386f8c7008ba9f4b57ead2
JianZcar <esteban.jianzcar@outlook.com>
2026-02-17 18:36:51 +0800

    fix?

--- build-scripts/base/02-nvidia.sh
+++ build-scripts/base/02-nvidia.sh
@@ -24,9 +24,8 @@ KVER=$(ls /usr/lib/modules | head -n1)
 
 dnf5 config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-nvidia.repo
 dnf5 config-manager setopt fedora-nvidia.enabled=0
-sed -i '/^enabled=/a\priority=90' /etc/yum.repos.d/fedora-nvidia.repo
 
-dnf5 -y install --enablerepo=fedora-nvidia akmod-nvidia --disablerepo="rpmfusion*"
+dnf5 -y install --enablerepo=fedora-nvidia akmod-nvidia --setopt=install_weak_deps=False
 
 mkdir -p /var/tmp
 chmod 1777 /var/tmp
@@ -34,14 +33,14 @@ chmod 1777 /var/tmp
 akmods --force --kernels "${KVER}" --kmod "nvidia"
 cat /var/cache/akmods/nvidia/*.failed.log || true
 
-dnf5 -y install --enablerepo=fedora-nvidia "${packages[@]}" --disablerepo="rpmfusion*"
+dnf5 -y install --enablerepo=fedora-nvidia "${packages[@]}" --setopt=install_weak_deps=False
 dnf5 versionlock add "${packages[@]}"
 
 dnf5 config-manager addrepo --from-repofile=https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo
 dnf5 config-manager setopt nvidia-container-toolkit.enabled=0
 dnf5 config-manager setopt nvidia-container-toolkit.gpgcheck=1
 
-dnf5 -y install --enablerepo=nvidia-container-toolkit \
+dnf5 -y install --enablerepo=nvidia-container-toolkit --setopt=install_weak_deps=False \
     nvidia-container-toolkit
 
 cp -a /ctx/nvidia/. /
