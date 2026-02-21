77259b32a737778c0490e49f0e9c2903eb3ebf5d
aceday <markcediebuday@gmail.com>
2026-02-21 13:02:04 +0800

    Migrated To kde.packages script

--- build-scripts/modules/de/kde/packages.sh
+++ build-scripts/modules/de/kde/packages.sh
@@ -6,3 +6,12 @@ packages=(
   @kde-desktop
 )
 dnf5 -y install "${packages[@]}"
+
+# Replace default icons with Zena icons
+TARGET_GLOB='/var/home/*/.config/plasma-org.kde.plasma.desktop-appletsrc'
+TARGET_ICON='/usr/share/icons/cachyos.svg'
+
+for file in $TARGET_GLOB; do
+  [[ -f "$file" ]] || continue
+  sed -i "s|^icon=.*|icon=${TARGET_ICON}|" "$file"
+done
