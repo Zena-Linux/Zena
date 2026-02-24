60b3eff3225dc0ceb83a785da6e55fbb3e5f6e84
aceday <markcediebuday@gmail.com>
2026-02-24 12:59:37 +0800

    Refractoring build

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -3,73 +3,43 @@ set -euo pipefail
 
 echo "=== Building: $IMAGE ==="
 
-modules=()
+modules=(
+  "base.dnf"
+  "base.kernel"
+  "base.packages"
+  "base.system"
+  "base.services"
+)
 
 case "$IMAGE" in
   zena)
     cp -avf "/ctx/system-files/twm/." /
     modules=(
-      "base.dnf"
-      "base.kernel"
-      "base.packages"
-      "base.system"
-      "base.services"
       "de.wm.packages"
       "de.wm.services"
-      "integrations.homed"
-      "integrations.nix"
-      "sign"
-      "initramfs"
     )
     ;;
   zena-nvidia)
     cp -avf "/ctx/system-files/twm/." /
     cp -avf "/ctx/system-files/nvidia/." /
     modules=(
-      "base.dnf"
-      "base.kernel"
-      "base.packages"
-      "base.system"
-      "base.services"
       "de.wm.packages"
       "de.wm.services"
-      "integrations.homed"
-      "integrations.nix"
       "integrations.nvidia"
-      "sign"
-      "initramfs"
     )
     ;;
   zena-kde)
     # cp -avf "/ctx/system-files/kde/." /
     modules=(
-      "base.dnf"
-      "base.kernel"
-      "base.packages"
-      "base.system"
-      "base.services"
       "de.kde.packages"
-      "integrations.homed"
-      "integrations.nix"
-      "sign"
-      "initramfs"
     )
     ;;
   zena-kde-nvidia)
     # cp -avf "/ctx/system-files/kde/." /
     cp -avf "/ctx/system-files/nvidia/." /
     modules=(
-      "base.dnf"
-      "base.kernel"
-      "base.packages"
-      "base.system"
-      "base.services"
       "de.kde.packages"
-      "integrations.homed"
-      "integrations.nix"
       "integrations.nvidia"
-      "sign"
-      "initramfs"
     )
     ;;
   *)
@@ -77,6 +47,13 @@ case "$IMAGE" in
     exit 1
     ;;
 esac
+
+modules+=(
+  "integrations.homed"
+  "integrations.nix"
+  "sign"
+  "initramfs"
+)
 
 for mod in "${modules[@]}"; do
     path="/ctx/modules/${mod//./\/}.sh"
