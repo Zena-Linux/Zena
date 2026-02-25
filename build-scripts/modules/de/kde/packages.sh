573d576945cc27a141263d3538198e57b41aafe2
aceday <markcediebuday@gmail.com>
2026-02-25 20:55:20 +0800

    Just Plasma Workspace first

--- build-scripts/modules/de/kde/packages.sh
+++ build-scripts/modules/de/kde/packages.sh
@@ -2,7 +2,9 @@ set -ouex pipefail
 
 shopt -s nullglob
 
-packages=(
-  @kde-desktop
-)
-dnf5 -y install "${packages[@]}"
+# packages=(
+#   @kde-desktop
+# )
+# dnf5 -y install "${packages[@]}"
+
+dnf group install "KDE Plasma Workspaces"
